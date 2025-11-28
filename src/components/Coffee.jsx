25c8d04335a6b20a031f2ff2a2e259428197f99a
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-28 15:50:45 +0100

    Implemented 3D Coffee Cup, though layout has been changed a lot

--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Cup } from "./models/CoffeeCup";
+import { OrbitControls } from "@react-three/drei";
 
-const Coffee = () => {
-  return (
-    <div>Coffee</div>
-  )
+function RotatingCup(props) {
+    const ref = React.useRef();
+    useFrame((state, delta) => {
+        ref.current.rotation.y += delta * 0.2;
+    });
+    return <Cup ref={ref} {...props} />;
 }
 
-export default Coffee
+export default function Coffee() {
+  return (
+    <Canvas
+      id="canvas"
+      className="w-1/2 h-full relative z-40"
+      camera={{ position: [0, 2, 5], fov: 50 }}
+    >
+        <OrbitControls enableZoom={false} enableDamping={true} dampingFactor={0.1}/>
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <RotatingCup scale={25} position={[0, 0, 0]} />
+    </Canvas>
+  );
+}
