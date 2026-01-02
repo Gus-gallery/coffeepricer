08aefba3541243f340c38e54caad8bb36ed4fd19
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-01-02 16:27:19 +0100

    Updated text in header

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div className="flex items-center justify-center p-4 shadow-md bg-border/10 backdrop-blur-md sticky top-0 z-50">
-        <h1 className="text-xs text-orange-300 font-extrabold">Global Coffee Price Index</h1>
+    <div className="flex-col items-center justify-center p-2 shadow-md bg-border/10 backdrop-blur-md sticky top-0 z-50">
+        <h1 className="text-xs text-center text-orange-300 font-extrabold">Global Coffee Price Index</h1>
     
+        <div className="text-xs text-center text-third font-regular p-1 -mb-2">
+          Project made by Gustav Werdelin for educational purposes.
+        </div>
     </div>
+    
   )
 }
 
